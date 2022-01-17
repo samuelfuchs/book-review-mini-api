cbc1318ac67d0237acfeac17fe1c4373c11dfc2b
Samuel Fuchs <fuchsgerson@gmail.com>
2022-01-17 03:42:19 -0300

    couple of 'mustread'-tags added

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -3,11 +3,11 @@ module.exports = (temp, product) => {
   output = output.replace(/{%IMAGE%}/g, product.image)
   output = output.replace(/{%PRICE%}/g, product.price)
   output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%TOPICS%}/g, product.nutrients)
+  output = output.replace(/{%TOPICS%}/g, product.topics)
   output = output.replace(/{%QUANTITY%}/g, product.quantity)
   output = output.replace(/{%DESCRIPTION%}/g, product.description)
   output = output.replace(/{%ID%}/g, product.id)
-  if (!product.organic)
+  if (!product.mustread)
     output = output.replace(/{%NOT_MUSTREAD%}/g, 'not-mustread')
 
   return output
